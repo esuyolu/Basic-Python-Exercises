cd23d753a9571f9f3ec2803c077417072496af63
Esma Suyolu <esmasuyolu@gmail.com>
2023-05-21 20:35:13 +0300

    Update h.001.1-sorting-list.py

--- h.001.1-sorting-list.py
+++ h.001.1-sorting-list.py
@@ -22,7 +22,7 @@ c = a[size:]
 a = c + b
 print(a)
 
-# If the list has an even number of items
+# If the list has an odd number of items
 
 a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
 
