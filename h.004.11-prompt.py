ea71aa1510d88076403c3df6914516a39434abab
Esma Suyolu <esmasuyolu@gmail.com>
2023-08-31 12:55:47 +0300

    fixes h.004.11-prompt.py

--- h.004.11-prompt.py
+++ h.004.11-prompt.py
@@ -1,28 +1,72 @@
 while True:
-    cmd = input('CSD>').split()
+    cmdline = input('CSD>').strip()
+
+    i = 0
+    while i < len(cmdline):
+        if cmdline[i].isspace():
+            break
+        i += 1
+
+    cmd = cmdline[:i]
+
     if len(cmd) == 0:
         continue
+
     match cmd:
-        case 'length', text:
-            print(len(text))
-        case 'upper', text:
-            print(text.upper())
-        case 'lower', text:
-            print(text.lower())
-        case 'concat', text1, text2:
-            print(text1.concat(text2))
-        case 'reverse', text:
-            print(text[::-1])
-        case 'repitition', text, n:
-            print(text * int(n))
-        case 'count', text:
-            d = dict()
-            for t in text:
-                c = text.count(t)
-                d[t] = c
-            for i in d:
-                print(i, ' => ', d[i])
-        case ['quit']:
+        case 'length' | 'reverse' | 'lower' | 'upper' | 'count' | 'repitition':
+            while i < len(cmdline) and cmdline[i].isspace():
+                i += 1
+            if i == len(cmdline) or cmdline[i] != '"':
+                print('invalid string')
+                continue
+            i += 1
+            start = i
+
+            while i < len(cmdline) and cmdline[i] != '"':
+                i += 1
+            if i == len(cmdline):
+                print('unterminated string')
+                continue
+
+            if cmd != 'repitition' and i != len(cmdline) - 1:
+                print('command has extra characters')
+                continue
+
+            s = cmdline[start:i]
+            if cmd == 'length':
+                print(i - start)
+            elif cmd == 'reverse':
+                print(s[::-1])
+            elif cmd == 'lower':
+                print(s.lower())
+            elif cmd == 'upper':
+                print(s.upper())
+            elif cmd == 'count':
+                for c in set(s):
+                    print(f'{c} => {s.count(c)}')
+            elif cmd == 'repitition':
+                i += 1
+                while i < len(cmdline) and cmdline[i].isspace():
+                    i += 1
+
+                if i == len(cmdline):
+                    print('repitition command without repitition count')
+                    continue
+
+                start = i
+                while i < len(cmdline) and cmdline[i].isdigit():
+                    i += 1
+
+                if i != len(cmdline):
+                    print('command count invalid')
+                    continue
+
+                n = int(cmdline[start:i])
+                print(s * n)
+
+        case 'reverse':
+            print('reverse command')
+        case 'quit':
             break
-        case _:
-            print(f'invalid command: {cmd[0]}')
+
+        
