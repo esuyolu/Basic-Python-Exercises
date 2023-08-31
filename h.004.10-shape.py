97ad6f7a908aa2c38b21cff049f41c6f0a6f6485
Esma Suyolu <esmasuyolu@gmail.com>
2023-08-31 12:47:40 +0300

    fixes h.004.10-shape.py

--- h.004.10-shape.py
+++ h.004.10-shape.py
@@ -14,24 +14,9 @@
 
 n = int(input('enter a number: '))
 
-for i in range(n):
-    for j in range(n - 1 - i):
-        print(' ', end='')
-    for k in range(2 * i + 1):
-        print('*', end='')
-    print()
+for i in range(1, n + 1):
+    print(' ' * (n - i) + '*' * (2 * i - 1))
 
-n -= 1
-i = 1
+for i in range(n - 1, 0, -1):
+    print(' ' * (n - i) + '*' * (2 * i - 1))
 
-while i <= n:
-    j = 1
-    while j <= i:
-        print(' ', end='')
-        j += 1
-    k = 1
-    while k <= (2 * (n - i) + 1):
-        print('*', end='')
-        k += 1
-    i += 1
-    print()
